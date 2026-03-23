3d7deb7d3e48a9d9225a55d93cd6c133f53a0d49
SDM Piraquara <sdmpiraquara2020@gmail.com>
2026-03-23 08:51:40 -0300

    build: adiciona assets compilados do frontend

---
+++ frontend/dist/assets/SectionToolbar-jVcHFdrp.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BtL2prAe.js";import{c as t}from"./clsx-B-dksMZM.js";function m({children:o,className:r}){return s.jsx("div",{className:t("toolbar",r),children:o})}export{m as S};
